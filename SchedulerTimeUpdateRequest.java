8efbba3f81ab2cf86ec4e538ed414b88dcdc7999
KOMAL BORADE <152424555+KomalBorade11@users.noreply.github.com>
2026-08-12 17:45:32 +0530

    Create SchedulerTimeUpdateRequest.java

---
+++ SchedulerTimeUpdateRequest.java
@@ -0,0 +1,14 @@
+package org.example.dto;
+
+public class SchedulerTimeUpdateRequest {
+
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
