10456555f6040253bdc5d222cc0e7b0028062fea
KOMAL BORADE <152424555+KomalBorade11@users.noreply.github.com>
2026-08-12 17:47:21 +0530

    Create BranchConsumerServiceApplication class

---
+++ BranchConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchConsumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchConsumerServiceApplication.class, args);
+
+    }
+}
